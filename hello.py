e3078a80336ea9eb2384bba481391d65f184e51d
JC <jcbcarbonel.ece@gmail.com>
2025-08-09 17:15:32 +0800

    chore: version 1

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello!")
+// Version 1 (Backward Compatible)
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
--- world.py
+++
@@ -1,4 +0,0 @@
-{
-  "user_id": "12",
-  "username": "john_doe"
-}
